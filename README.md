9940c632dff5d9a60744378467179c554777b60c
Jasdeep singh <narang.jasdeep@gmail.com>
2013-08-14 17:46:37 -0400

    updated readme, added footer links etc

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-## This is readme file
+## Angular Invoicing
 
+Use this extremely small and lightweight project built in Angular JS to build your one off Invoices. No monthly fees, No Subscriptions - No heavy duty features.
+
+## Usage
+
+Use this project here: (link here) or clone it and customize it to your hearts content.
+
+## Feature Requests
+
+Feel free to open any pull requests if you have any feature requests.
+
+## Contributors
+
+* [Manpreet Singh](http://github.com/manpreetrules)
+* [Jasdeep Singh](http://jasdeep.ca)
+
+## License
+
+No license restrictions, use it however and wherever you want - Commercial, Personal, Non-profit however you please. Just send us a quick note letting us know this project came handy and we'll be flattered. :)
--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 function InvoiceController($scope) {
 
-	$scope.logoRemoved = false;
-	$scope.printMode = false;
+  $scope.logoRemoved = false;
+  $scope.printMode = false;
 
-	$scope.invoice = {
-					  tax: 13.00, 
-					  invoice_number: 10,
-					  customer_info:  {name: "Mr. Manpreet Narang", web_link: "Tauxik Design Labs", address1: "49 Avalanche Cres.", address2: "Toronto, ON, Canada", postal: "L6P1L8"},
-					  company_info:  {name: "Metaware Labs", web_link: "www.metawarelabs.com", address1: "49 Avalanche Cres.", address2: "Toronto, ON, Canada", postal: "L6P1L8"},
-				      items:[ {qty:10, description:'Gadget', cost:9.95}]}
+  $scope.invoice = {
+            tax: 13.00, 
+            invoice_number: 10,
+            customer_info:  {name: "Mr. John Doe", web_link: "John Doe Designs Inc.", address1: "1 Infinite Loop", address2: "Cupertino, California, US", postal: "90210"},
+            company_info:  {name: "Metaware Labs", web_link: "www.metawarelabs.com", address1: "123 Yonge Street", address2: "Toronto, ON, Canada", postal: "M5S 1B6"},
+              items:[ {qty:10, description:'Gadget', cost:9.95}]}
 
     $scope.addItem = function() {
         $scope.invoice.items.push({qty:0, cost:0, description:""});    
@@ -16,18 +16,18 @@ function InvoiceController($scope) {
     $scope.removeLogo = function(element) {
         var elem = angular.element("#remove_logo");
         if(elem.text() == "Show Logo"){
-        	elem.text("Remove Logo");
-        	$scope.logoRemoved = false;
+          elem.text("Remove Logo");
+          $scope.logoRemoved = false;
         }
         else{
-        	elem.text("Show Logo");
-	        $scope.logoRemoved = true;
+          elem.text("Show Logo");
+          $scope.logoRemoved = true;
         }
 
     }
 
     $scope.editLogo = function(){
-    	$("#imgInp").trigger("click");
+      $("#imgInp").trigger("click");
     }
 
     $scope.showLogo = function() {
@@ -40,15 +40,15 @@ function InvoiceController($scope) {
         var total = 0.00;
         console.log($scope.invoice.items);
         angular.forEach($scope.invoice.items, function(item, key){
-			total += (item.qty * item.cost);
-		});
+      total += (item.qty * item.cost);
+    });
         return total;
     }
     $scope.invoice_sub_total = function() {
         var total = 0.00;
         angular.forEach($scope.invoice.items, function(item, key){
-			total += (item.qty * item.cost);
-		});
+      total += (item.qty * item.cost);
+    });
         return total;
     }
     $scope.calculate_tax = function() {
@@ -59,9 +59,9 @@ function InvoiceController($scope) {
     } 
 
     $scope.printInfo = function() {
-    	window.print();
-		$scope.printMode = false;
-	}
+      window.print();
+    $scope.printMode = false;
+  }
 
 
 };
@@ -83,7 +83,7 @@ function readURL(input) {
 }
 
 $(document).ready(function(){
-	$("#imgInp").change(function(){
-		readURL(this);
-	});
+  $("#imgInp").change(function(){
+    readURL(this);
+  });
 });
