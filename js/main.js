d3b0862ef5331fdd4a2e077f7c60bcb5bec236ac
Manpreet <manpreet@webdesignerintoronto.com>
2013-08-15 17:31:52 -0400

    suggestions - HN

--- js/main.js
+++ js/main.js
@@ -3,13 +3,19 @@ function InvoiceController($scope) {
   $scope.logoRemoved = false;
   $scope.printMode = false;
 
-  $scope.invoice = {
+  var sample_invoice = {
             tax: 13.00, 
             invoice_number: 10,
             customer_info:  {name: "Mr. John Doe", web_link: "John Doe Designs Inc.", address1: "1 Infinite Loop", address2: "Cupertino, California, US", postal: "90210"},
             company_info:  {name: "Metaware Labs", web_link: "www.metawarelabs.com", address1: "123 Yonge Street", address2: "Toronto, ON, Canada", postal: "M5S 1B6"},
-              items:[ {qty:10, description:'Gadget', cost:9.95}]}
+              items:[ {qty:10, description:'Gadget', cost:9.95}]};
 
+  if(localStorage["invoice"] == "" || localStorage["invoice"] == null){
+    $scope.invoice = sample_invoice;
+  }
+  else{
+    $scope.invoice =  JSON.parse(localStorage["invoice"]);
+  }
     $scope.addItem = function() {
         $scope.invoice.items.push({qty:0, cost:0, description:""});    
     }
@@ -36,31 +42,33 @@ function InvoiceController($scope) {
     $scope.removeItem = function(item) {
         $scope.invoice.items.splice($scope.invoice.items.indexOf(item), 1);    
     }
-    $scope.invoice_sub_total = function() {
-        var total = 0.00;
-        console.log($scope.invoice.items);
-        angular.forEach($scope.invoice.items, function(item, key){
-      total += (item.qty * item.cost);
-    });
-        return total;
-    }
+    
     $scope.invoice_sub_total = function() {
         var total = 0.00;
         angular.forEach($scope.invoice.items, function(item, key){
-      total += (item.qty * item.cost);
-    });
+          total += (item.qty * item.cost);
+        });
         return total;
     }
     $scope.calculate_tax = function() {
         return (($scope.invoice.tax * $scope.invoice_sub_total())/100);
     }
     $scope.calculate_grand_total = function() {
+        localStorage["invoice"] = JSON.stringify($scope.invoice);
         return $scope.calculate_tax() + $scope.invoice_sub_total();
     } 
 
     $scope.printInfo = function() {
       window.print();
-  }
+    }
+
+    $scope.clearLocalStorage = function(){
+      var confirmClear = confirm("Are you sure you would like to clear the invoice?");
+      if(confirmClear){
+        localStorage["invoice"] = "";
+        $scope.invoice = sample_invoice;
+      }
+    }
 
 
 };
@@ -81,7 +89,12 @@ function readURL(input) {
     }
 }
 
+// window.onbeforeunload = function(e) {
+//   confirm('Are you sure you would like to close this tab? All your data will be lost');
+// };
+
 $(document).ready(function(){
+  $("#invoice_number").focus();
   $("#imgInp").change(function(){
     readURL(this);
   });
