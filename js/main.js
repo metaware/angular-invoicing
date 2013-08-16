570b89a3486fd91c6897d904fe88c5438270fc17
William Yaworsky <wj.px01@gmail.com>
2013-08-16 12:57:25 -0400

    Fixed some html/js formatting
    Added HTML5 doctype
    HTML attributes use double quotes (")
    JavaScript strings use double quotes (except google analytics)
    JavaScript indentations are 2 spaces
    Made sample invoice more readable

--- js/main.js
+++ js/main.js
@@ -4,11 +4,26 @@ function InvoiceController($scope) {
   $scope.printMode = false;
 
   var sample_invoice = {
-            tax: 13.00,
-            invoice_number: 10,
-            customer_info:  {name: "Mr. John Doe", web_link: "John Doe Designs Inc.", address1: "1 Infinite Loop", address2: "Cupertino, California, US", postal: "90210"},
-            company_info:  {name: "Metaware Labs", web_link: "www.metawarelabs.com", address1: "123 Yonge Street", address2: "Toronto, ON, Canada", postal: "M5S 1B6"},
-              items:[ {qty:10, description:'Gadget', cost:9.95}]};
+    tax: 13.00,
+    invoice_number: 10,
+    customer_info: {
+      name: "Mr. John Doe",
+      web_link: "John Doe Designs Inc.",
+      address1: "1 Infinite Loop",
+      address2: "Cupertino, California, US",
+      postal: "90210"
+    },
+    company_info: {
+      name: "Metaware Labs",
+      web_link: "www.metawarelabs.com",
+      address1: "123 Yonge Street",
+      address2: "Toronto, ON, Canada",
+      postal: "M5S 1B6"
+    },
+    items:[
+      { qty: 10, description: "Gadget", cost: 9.95 }
+    ]
+  };
 
   var default_logo = "images/metaware_logo.png";
 
@@ -25,88 +40,90 @@ function InvoiceController($scope) {
     $scope.logo = default_logo;
   }
 
-    $scope.addItem = function() {
-        $scope.invoice.items.push({qty:0, cost:0, description:""});
-    }
-    $scope.removeLogo = function(element) {
-        var elem = angular.element("#remove_logo");
-        if(elem.text() == "Show Logo"){
-          elem.text("Remove Logo");
-          $scope.logoRemoved = false;
-        }
-        else{
-          elem.text("Show Logo");
-          $scope.logoRemoved = true;
-        }
-        localStorage["logo"] = "";
-    }
+  $scope.addItem = function() {
+    $scope.invoice.items.push({ qty:0, cost:0, description:"" });
+  };
 
-    $scope.editLogo = function(){
-      $("#imgInp").trigger("click");
+  $scope.removeLogo = function(element) {
+    var elem = angular.element("#remove_logo");
+    if(elem.text() == "Show Logo"){
+      elem.text("Remove Logo");
+      $scope.logoRemoved = false;
+    } else {
+      elem.text("Show Logo");
+      $scope.logoRemoved = true;
     }
+    localStorage["logo"] = "";
+  };
 
-    $scope.showLogo = function() {
-        $scope.logoRemoved = false;
-    }
-    $scope.removeItem = function(item) {
-        $scope.invoice.items.splice($scope.invoice.items.indexOf(item), 1);
-    }
+  $scope.editLogo = function() {
+    $("#imgInp").trigger("click");
+  };
 
-    $scope.invoice_sub_total = function() {
-        var total = 0.00;
-        angular.forEach($scope.invoice.items, function(item, key){
-          total += (item.qty * item.cost);
-        });
-        return total;
-    }
-    $scope.calculate_tax = function() {
-        return (($scope.invoice.tax * $scope.invoice_sub_total())/100);
-    }
-    $scope.calculate_grand_total = function() {
-        localStorage["invoice"] = JSON.stringify($scope.invoice);
-        return $scope.calculate_tax() + $scope.invoice_sub_total();
-    }
+  $scope.showLogo = function() {
+    $scope.logoRemoved = false;
+  };
 
-    $scope.printInfo = function() {
-      window.print();
-    }
+  $scope.removeItem = function(item) {
+    $scope.invoice.items.splice($scope.invoice.items.indexOf(item), 1);
+  };
 
-    $scope.clearLocalStorage = function(){
-      var confirmClear = confirm("Are you sure you would like to clear the invoice?");
-      if(confirmClear){
-        localStorage["invoice"] = "";
-        localStorage["logo"] = "";
-        $scope.invoice = sample_invoice;
-      }
-    }
+  $scope.invoice_sub_total = function() {
+    var total = 0.00;
+    angular.forEach($scope.invoice.items, function(item, key){
+      total += (item.qty * item.cost);
+    });
+    return total;
+  };
 
+  $scope.calculate_tax = function() {
+    return (($scope.invoice.tax * $scope.invoice_sub_total())/100);
+  };
+
+  $scope.calculate_grand_total = function() {
+    localStorage["invoice"] = JSON.stringify($scope.invoice);
+    return $scope.calculate_tax() + $scope.invoice_sub_total();
+  };
+
+  $scope.printInfo = function() {
+    window.print();
+  };
+
+  $scope.clearLocalStorage = function() {
+    var confirmClear = confirm("Are you sure you would like to clear the invoice?");
+    if(confirmClear){
+      localStorage["invoice"] = "";
+      localStorage["logo"] = "";
+      $scope.invoice = sample_invoice;
+    }
+  };
 
 };
 
-angular.module('jqanim', []).directive('jqAnimate', function(){
+angular.module('jqanim', []).directive('jqAnimate', function() {
   return function(scope, instanceElement){
-      setTimeout(function() {instanceElement.show('slow');}, 0);
+    setTimeout(function() {instanceElement.show('slow');}, 0);
   }
 });
 
 function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            $('#company_logo').attr('src', e.target.result);
-            localStorage["logo"] = e.target.result;
-        }
-        reader.readAsDataURL(input.files[0]);
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $('#company_logo').attr('src', e.target.result);
+      localStorage["logo"] = e.target.result;
     }
+    reader.readAsDataURL(input.files[0]);
+  }
 }
 
 // window.onbeforeunload = function(e) {
 //   confirm('Are you sure you would like to close this tab? All your data will be lost');
 // };
 
-$(document).ready(function(){
+$(document).ready(function() {
   $("#invoice_number").focus();
-  $("#imgInp").change(function(){
+  $("#imgInp").change(function() {
     readURL(this);
   });
 });
