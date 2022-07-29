909572de03a54fd489874cdb053971be80dbad5e
jgbpessoa <jgbpessoa@gmail.com>
2022-07-29 16:33:49 -0300

    Set up Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { Counter } from "./Counter"
 
 function App() {
   return (
-    <div>
-      <h1>React TypeScript Webpack - {process.env.NODE_ENV}</h1>
-      <img src="./images/example.png" alt="Woman listening to music" />
-      <img
-        src="./images/example.svg"
-        alt="Woman listening to music"
-        width={100}
-      />
-      <Counter />
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="text-md-center">
+            React TypeScript Webpack - {process.env.NODE_ENV}
+          </h1>
+        </div>
+        <div className="col">
+          <img src="./images/logo.svg" alt="Ploomes logo" />
+        </div>
+        <Counter />
+      </div>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css"
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
